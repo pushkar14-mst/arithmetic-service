3867758530aa2bf5dbf75cee9ee9dbff13bea3df
Pushkar Patil <pushkar@Pushkars-MacBook-Pro.local>
2024-01-18 19:45:41 -0500

    c2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("sum", (req, res) => {
+app.post("/", (req, res) => {
   let num1 = parseInt(req.body.num1);
   let num2 = parseInt(req.body.num2);
   let sum = num1 + num2;
