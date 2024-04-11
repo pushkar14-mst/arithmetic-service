8c835e6d2100faf50afca0bb43428561d8d268c0
Pushkar Patil <pushkar@Pushkars-MacBook-Pro.local>
2024-04-11 17:01:30 -0400

    added deploy.yml for kubernetes

--- index.js
+++ index.js
@@ -2,16 +2,26 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const dotenv = require("dotenv");
 const { add } = require("./arithmetica");
+dotenv.config();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+if (!process.env.PORT) {
+  throw new Error("PORT is not defined");
+}
+
+const port = process.env.PORT;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.get("/add/:n1/:n2", (req, res) => {
   let num1 = parseInt(req.params.n1);
   let num2 = parseInt(req.params.n2);
   let sum = add(num1, num2);
   res.json({ sum: sum });
 });
-app.listen(8080, () => {
-  console.log("Server started on port 8080");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
